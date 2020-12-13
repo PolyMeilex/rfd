11a2903d0c08340d2c9c5e8b39857bb2da81d25e
PolyMeilex <marynczakbartlomiej@gmail.com>
2020-12-12 17:34:19 -0800

    [MacOs] Cleanup

--- src/backend/macos.rs
+++ src/backend/macos.rs
@@ -2,170 +2,180 @@ use crate::DialogOptions;
 use std::path::Path;
 use std::path::PathBuf;
 
-use objc::{msg_send, sel, sel_impl};
+use objc::{class, msg_send, sel, sel_impl};
 
+use cocoa_foundation::base::id;
 use cocoa_foundation::base::nil;
-use cocoa_foundation::foundation::NSAutoreleasePool;
-pub use objc::runtime::{BOOL, NO, YES};
+use cocoa_foundation::foundation::{NSArray, NSAutoreleasePool, NSString, NSURL};
+use objc::runtime::{Object, YES};
+pub use objc::runtime::{BOOL, NO};
 
-mod utils {
-    use crate::DialogOptions;
-    use std::path::Path;
+pub fn pick_file<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<PathBuf> {
+    let opt = params.into().unwrap_or_default();
 
-    use std::path::PathBuf;
+    let pool = unsafe { NSAutoreleasePool::new(nil) };
+    let panel = Panel::open_panel();
 
-    use cocoa_foundation::base::{id, nil};
-    use cocoa_foundation::foundation::{NSArray, NSAutoreleasePool, NSString, NSURL};
-    use objc::runtime::{Object, YES};
-    use objc::{class, msg_send, sel, sel_impl};
-
-    extern "C" {
-        pub fn CGShieldingWindowLevel() -> i32;
+    if !opt.filters.is_empty() {
+        panel.add_filters(&opt);
     }
 
-    pub unsafe fn app() -> *mut Object {
-        msg_send![class!(NSApplication), sharedApplication]
+    if let Some(path) = &opt.starting_directory {
+        panel.set_path(path);
     }
 
-    pub unsafe fn key_window() -> *mut Object {
-        let app = app();
-        msg_send![app, keyWindow]
-    }
+    panel.set_can_choose_directories(NO);
+    panel.set_can_choose_files(YES);
 
-    pub fn open_panel() -> *mut Object {
-        unsafe { msg_send![class!(NSOpenPanel), openPanel] }
-    }
+    let res = if panel.run_modal() == 1 {
+        Some(panel.get_result())
+    } else {
+        None
+    };
 
-    pub fn save_panel() -> *mut Object {
-        unsafe { msg_send![class!(NSSavePanel), savePanel] }
-    }
+    unsafe { pool.drain() };
 
-    pub fn make_nsstring(s: &str) -> id {
-        unsafe { NSString::alloc(nil).init_str(s).autorelease() }
-    }
-
-    pub unsafe fn add_filters(panel: id, params: &DialogOptions) {
-        let mut exts: Vec<&str> = Vec::new();
-
-        for filter in params.filters.iter() {
-            exts.append(&mut filter.extensions.to_vec());
-        }
-
-        let f_raw: Vec<_> = exts.iter().map(|ext| make_nsstring(ext)).collect();
-
-        let array = NSArray::arrayWithObjects(nil, f_raw.as_slice());
-        let _: () = msg_send![panel, setAllowedFileTypes: array];
-    }
-
-    pub unsafe fn set_path(panel: id, path: &Path) {
-        if let Some(path) = path.to_str() {
-            let url = NSURL::alloc(nil)
-                .initFileURLWithPath_isDirectory_(make_nsstring(path), YES)
-                .autorelease();
-            let () = msg_send![panel, setDirectoryURL: url];
-        }
-    }
-
-    pub unsafe fn get_result(panel: id) -> PathBuf {
-        let url: id = msg_send![panel, URL];
-        let path: id = msg_send![url, path];
-        let utf8: *const i32 = msg_send![path, UTF8String];
-        let len: usize = msg_send![path, lengthOfBytesUsingEncoding:4 /*UTF8*/];
-
-        let slice = std::slice::from_raw_parts(utf8 as *const _, len);
-        let result = std::str::from_utf8_unchecked(slice);
-
-        result.into()
-    }
-
-    pub unsafe fn get_results(panel: id) -> Vec<PathBuf> {
-        let urls: id = msg_send![panel, URLs];
-
-        let count = urls.count();
-
-        let mut res = Vec::new();
-        for id in 0..count {
-            let url = urls.objectAtIndex(id);
-            let path: id = msg_send![url, path];
-            let utf8: *const i32 = msg_send![path, UTF8String];
-            let len: usize = msg_send![path, lengthOfBytesUsingEncoding:4 /*UTF8*/];
-
-            let slice = std::slice::from_raw_parts(utf8 as *const _, len);
-            let result = std::str::from_utf8_unchecked(slice);
-            res.push(result.into());
-        }
-
-        res
-    }
-
-    #[repr(i32)]
-    #[derive(Debug, PartialEq)]
-    enum ApplicationActivationPolicy {
-        //Regular = 0,
-        Accessory = 1,
-        Prohibited = 2,
-        //Error = -1,
-    }
-
-    pub struct AppPolicyManager {
-        initial_policy: i32,
-    }
-
-    impl AppPolicyManager {
-        pub fn new() -> Self {
-            unsafe {
-                let app = app();
-                let initial_policy: i32 = msg_send![app, activationPolicy];
-
-                if initial_policy == ApplicationActivationPolicy::Prohibited as i32 {
-                    let new_pol = ApplicationActivationPolicy::Accessory as i32;
-                    let _: () = msg_send![app, setActivationPolicy: new_pol];
-                }
-
-                Self { initial_policy }
-            }
-        }
-    }
-    impl Drop for AppPolicyManager {
-        fn drop(&mut self) {
-            unsafe {
-                let app = app();
-                // Restore initial pol
-                let _: () = msg_send![app, setActivationPolicy: self.initial_policy];
-            }
-        }
-    }
+    res
 }
 
-use utils::*;
+pub fn save_file<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<PathBuf> {
+    let opt = params.into().unwrap_or_default();
+
+    let pool = unsafe { NSAutoreleasePool::new(nil) };
+    let panel = Panel::save_panel();
+
+    if let Some(path) = &opt.starting_directory {
+        panel.set_path(path);
+    }
+
+    let res = if panel.run_modal() == 1 {
+        Some(panel.get_result())
+    } else {
+        None
+    };
+
+    unsafe { pool.drain() };
+
+    res
+}
+
+pub fn pick_folder<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<PathBuf> {
+    let opt = params.into().unwrap_or_default();
+
+    let pool = unsafe { NSAutoreleasePool::new(nil) };
+    let panel = Panel::open_panel();
+
+    if let Some(path) = &opt.starting_directory {
+        panel.set_path(path);
+    }
+
+    panel.set_can_choose_directories(YES);
+    panel.set_can_choose_files(NO);
+
+    let res = if panel.run_modal() == 1 {
+        Some(panel.get_result())
+    } else {
+        None
+    };
+
+    unsafe { pool.drain() };
+
+    res
+}
+
+pub fn pick_files<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<Vec<PathBuf>> {
+    let opt = params.into().unwrap_or_default();
+
+    let pool = unsafe { NSAutoreleasePool::new(nil) };
+    let panel = Panel::open_panel();
+
+    if !opt.filters.is_empty() {
+        panel.add_filters(&opt);
+    }
+
+    if let Some(path) = &opt.starting_directory {
+        panel.set_path(path);
+    }
+
+    panel.set_can_choose_directories(NO);
+    panel.set_can_choose_files(YES);
+    panel.set_allows_multiple_selection(YES);
+
+    let res = if panel.run_modal() == 1 {
+        Some(panel.get_results())
+    } else {
+        None
+    };
+
+    unsafe { pool.drain() };
+
+    res
+}
+
+//
+// Internal
+//
+
+extern "C" {
+    pub fn CGShieldingWindowLevel() -> i32;
+}
+fn make_nsstring(s: &str) -> id {
+    unsafe { NSString::alloc(nil).init_str(s).autorelease() }
+}
 
 struct Panel {
     panel: *mut Object,
-    policy_manager: AppPolicyManager,
+    _policy_manager: AppPolicyManager,
+    key_window: *mut Object,
 }
 impl Panel {
     fn new(panel: *mut Object) -> Self {
-        let policy_manager = AppPolicyManager::new();
-        let _: () = msg_send![panel, setLevel: CGShieldingWindowLevel()];
-        Self { policy_manager, panel }
+        let _policy_manager = AppPolicyManager::new();
+        let key_window = unsafe {
+            let app: *mut Object = msg_send![class!(NSApplication), sharedApplication];
+            msg_send![app, keyWindow]
+        };
+
+        let _: () = unsafe { msg_send![panel, setLevel: CGShieldingWindowLevel()] };
+        Self {
+            _policy_manager,
+            panel,
+            key_window,
+        }
     }
 
     fn open_panel() -> Self {
         Self::new(unsafe { msg_send![class!(NSOpenPanel), openPanel] })
     }
 
-    fn save_panel() -> *mut Object {
+    fn save_panel() -> Self {
         Self::new(unsafe { msg_send![class!(NSSavePanel), savePanel] })
     }
 
-    fn add_filters(&self,params: &DialogOptions) {
-        unsafe{
-            let mut exts: Vec<&str> = Vec::new();
+    fn run_modal(&self) -> i32 {
+        unsafe { msg_send![self.panel, runModal] }
+    }
 
-            for filter in params.filters.iter() {
-                exts.append(&mut filter.extensions.to_vec());
-            }
+    fn set_can_choose_directories(&self, v: BOOL) {
+        let _: () = unsafe { msg_send![self.panel, setCanChooseDirectories: v] };
+    }
 
+    fn set_can_choose_files(&self, v: BOOL) {
+        let _: () = unsafe { msg_send![self.panel, setCanChooseFiles: v] };
+    }
+
+    fn set_allows_multiple_selection(&self, v: BOOL) {
+        let _: () = unsafe { msg_send![self.panel, setAllowsMultipleSelection: v] };
+    }
+
+    fn add_filters(&self, params: &DialogOptions) {
+        let mut exts: Vec<&str> = Vec::new();
+
+        for filter in params.filters.iter() {
+            exts.append(&mut filter.extensions.to_vec());
+        }
+
+        unsafe {
             let f_raw: Vec<_> = exts.iter().map(|ext| make_nsstring(ext)).collect();
 
             let array = NSArray::arrayWithObjects(nil, f_raw.as_slice());
@@ -173,217 +183,94 @@ impl Panel {
         }
     }
 
-    fn set_path(&self,path: &Path) {
-        unsafe{
+    fn set_path(&self, path: &Path) {
         if let Some(path) = path.to_str() {
-            let url = NSURL::alloc(nil)
-                .initFileURLWithPath_isDirectory_(make_nsstring(path), YES)
-                .autorelease();
-            let () = msg_send![self.panel, setDirectoryURL: url];
-        }
+            unsafe {
+                let url = NSURL::alloc(nil)
+                    .initFileURLWithPath_isDirectory_(make_nsstring(path), YES)
+                    .autorelease();
+                let () = msg_send![self.panel, setDirectoryURL: url];
+            }
         }
     }
 
     fn get_result(&self) -> PathBuf {
-        unsafe{
-        let url: id = msg_send![self.panel, URL];
-        let path: id = msg_send![url, path];
-        let utf8: *const i32 = msg_send![path, UTF8String];
-        let len: usize = msg_send![path, lengthOfBytesUsingEncoding:4 /*UTF8*/];
-
-        let slice = std::slice::from_raw_parts(utf8 as *const _, len);
-        let result = std::str::from_utf8_unchecked(slice);
-
-        result.into()
-        }
-    }
-
-    fn get_results(&self) -> Vec<PathBuf> {
-        unsafe{
-        let urls: id = msg_send![self.panel, URLs];
-
-        let count = urls.count();
-
-        let mut res = Vec::new();
-        for id in 0..count {
-            let url = urls.objectAtIndex(id);
+        unsafe {
+            let url: id = msg_send![self.panel, URL];
             let path: id = msg_send![url, path];
             let utf8: *const i32 = msg_send![path, UTF8String];
             let len: usize = msg_send![path, lengthOfBytesUsingEncoding:4 /*UTF8*/];
 
             let slice = std::slice::from_raw_parts(utf8 as *const _, len);
             let result = std::str::from_utf8_unchecked(slice);
-            res.push(result.into());
-        }
 
-        res
+            result.into()
         }
     }
-}
 
-pub fn pick_file<'a>(mut params: impl Into<Option<DialogOptions<'a>>>) -> Option<PathBuf> {
-    let params = params.into().unwrap_or_default();
+    fn get_results(&self) -> Vec<PathBuf> {
+        unsafe {
+            let urls: id = msg_send![self.panel, URLs];
 
-    unsafe {
-        let pool = NSAutoreleasePool::new(nil);
+            let count = urls.count();
 
-        let key_window = key_window();
+            let mut res = Vec::new();
+            for id in 0..count {
+                let url = urls.objectAtIndex(id);
+                let path: id = msg_send![url, path];
+                let utf8: *const i32 = msg_send![path, UTF8String];
+                let len: usize = msg_send![path, lengthOfBytesUsingEncoding:4 /*UTF8*/];
 
-        let _policy_manager = AppPolicyManager::new();
-
-        let res = {
-            let panel = open_panel();
-
-            let _: () = msg_send![panel, setLevel: CGShieldingWindowLevel()];
-
-            let _: () = msg_send![panel, setCanChooseDirectories: NO];
-            let _: () = msg_send![panel, setCanChooseFiles: YES];
-
-            if !params.filters.is_empty() {
-                add_filters(panel, &params);
+                let slice = std::slice::from_raw_parts(utf8 as *const _, len);
+                let result = std::str::from_utf8_unchecked(slice);
+                res.push(result.into());
             }
 
-            if let Some(path) = &params.starting_directory {
-                set_path(panel, path);
-            }
-
-            let res: i32 = msg_send![panel, runModal];
-
-            if res == 1 {
-                Some(get_result(panel))
-            } else {
-                None
-            }
-        };
-
-        let _: () = msg_send![key_window, makeKeyAndOrderFront: nil];
-
-        pool.drain();
-
-        res
+            res
+        }
     }
 }
 
-pub fn save_file<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<PathBuf> {
-    let params = params.into().unwrap_or_default();
-
-    unsafe {
-        let pool = NSAutoreleasePool::new(nil);
-
-        let key_window = key_window();
-
-        let _policy_manager = AppPolicyManager::new();
-
-        let res = {
-            let panel = save_panel();
-
-            let _: () = msg_send![panel, setLevel: CGShieldingWindowLevel()];
-
-            // Save filters are unsupported on macos
-            //if !params.filters.is_empty() {
-            //add_filters(panel, &params);
-            //}
-
-            if let Some(path) = &params.starting_directory {
-                set_path(panel, path);
-            }
-
-            let res: i32 = msg_send![panel, runModal];
-
-            if res == 1 {
-                Some(get_result(panel))
-            } else {
-                None
-            }
-        };
-
-        let _: () = msg_send![key_window, makeKeyAndOrderFront: nil];
-
-        pool.drain();
-
-        res
+impl Drop for Panel {
+    fn drop(&mut self) {
+        let _: () = unsafe { msg_send![self.key_window, makeKeyAndOrderFront: nil] };
     }
 }
 
-pub fn pick_folder<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<PathBuf> {
-    let params = params.into().unwrap_or_default();
-
-    unsafe {
-        let pool = NSAutoreleasePool::new(nil);
-
-        let key_window = key_window();
-
-        let _policy_manager = AppPolicyManager::new();
-
-        let res = {
-            let panel = open_panel();
-
-            let _: () = msg_send![panel, setLevel: CGShieldingWindowLevel()];
-
-            let _: () = msg_send![panel, setCanChooseDirectories: YES];
-            let _: () = msg_send![panel, setCanChooseFiles: NO];
-
-            if let Some(path) = &params.starting_directory {
-                set_path(panel, path);
-            }
-
-            let res: i32 = msg_send![panel, runModal];
-
-            if res == 1 {
-                Some(get_result(panel))
-            } else {
-                None
-            }
-        };
-
-        let _: () = msg_send![key_window, makeKeyAndOrderFront: nil];
-
-        pool.drain();
-
-        res
-    }
+pub struct AppPolicyManager {
+    initial_policy: i32,
 }
 
-pub fn pick_files<'a>(params: impl Into<Option<DialogOptions<'a>>>) -> Option<Vec<PathBuf>> {
-    let params = params.into().unwrap_or_default();
+impl AppPolicyManager {
+    pub fn new() -> Self {
+        #[repr(i32)]
+        #[derive(Debug, PartialEq)]
+        enum ApplicationActivationPolicy {
+            //Regular = 0,
+            Accessory = 1,
+            Prohibited = 2,
+            //Error = -1,
+        }
 
-    unsafe {
-        let pool = NSAutoreleasePool::new(nil);
+        unsafe {
+            let app: *mut Object = msg_send![class!(NSApplication), sharedApplication];
+            let initial_policy: i32 = msg_send![app, activationPolicy];
 
-        let key_window = key_window();
-
-        let _policy_manager = AppPolicyManager::new();
-
-        let res = {
-            let panel = open_panel();
-
-            let _: () = msg_send![panel, setLevel: CGShieldingWindowLevel()];
-
-            let _: () = msg_send![panel, setCanChooseDirectories: NO];
-            let _: () = msg_send![panel, setCanChooseFiles: YES];
-            let _: () = msg_send![panel, setAllowsMultipleSelection: YES];
-
-            if !params.filters.is_empty() {
-                add_filters(panel, &params);
+            if initial_policy == ApplicationActivationPolicy::Prohibited as i32 {
+                let new_pol = ApplicationActivationPolicy::Accessory as i32;
+                let _: () = msg_send![app, setActivationPolicy: new_pol];
             }
 
-            if let Some(path) = &params.starting_directory {
-                set_path(panel, path);
-            }
-
-            let res: i32 = msg_send![panel, runModal];
-
-            if res == 1 {
-                Some(get_results(panel))
-            } else {
-                None
-            }
-        };
-
-        let _: () = msg_send![key_window, makeKeyAndOrderFront: nil];
-
-        pool.drain();
-
-        res
+            Self { initial_policy }
+        }
+    }
+}
+impl Drop for AppPolicyManager {
+    fn drop(&mut self) {
+        unsafe {
+            let app: *mut Object = msg_send![class!(NSApplication), sharedApplication];
+            // Restore initial pol
+            let _: () = msg_send![app, setActivationPolicy: self.initial_policy];
+        }
     }
 }
