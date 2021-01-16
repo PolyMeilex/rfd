5303fc494df89b3428a458dfde67c79c4d2069fd
Poly <marynczak.bartlomiej@gmail.com>
2021-01-16 18:28:35 +0100

    [Mac] Fix Build

--- src/backend/macos.rs
+++ src/backend/macos.rs
@@ -13,58 +13,58 @@ mod async_dialog;
 use async_dialog::AsyncDialog;
 pub use async_dialog::DialogFuture;
 
-pub fn pick_file<'a>(opt: &FileDialog<'a>) -> Option<PathBuf> {
+pub fn pick_file(opt: FileDialog) -> Option<PathBuf> {
     objc::rc::autoreleasepool(move || {
-        let panel = Panel::build_pick_file(opt);
+        let panel = Panel::build_pick_file(&opt);
 
         let res = panel.run_modal();
         OutputFrom::from(&panel, res)
     })
 }
 
-pub fn save_file<'a>(opt: &FileDialog<'a>) -> Option<PathBuf> {
+pub fn save_file(opt: FileDialog) -> Option<PathBuf> {
     objc::rc::autoreleasepool(move || {
-        let panel = Panel::build_save_file(opt);
+        let panel = Panel::build_save_file(&opt);
 
         let res = panel.run_modal();
         OutputFrom::from(&panel, res)
     })
 }
 
-pub fn pick_folder<'a>(opt: &FileDialog<'a>) -> Option<PathBuf> {
+pub fn pick_folder(opt: FileDialog) -> Option<PathBuf> {
     objc::rc::autoreleasepool(move || {
-        let panel = Panel::build_pick_folder(opt);
+        let panel = Panel::build_pick_folder(&opt);
 
         let res = panel.run_modal();
         OutputFrom::from(&panel, res)
     })
 }
 
-pub fn pick_files<'a>(opt: &FileDialog<'a>) -> Option<Vec<PathBuf>> {
+pub fn pick_files(opt: FileDialog) -> Option<Vec<PathBuf>> {
     objc::rc::autoreleasepool(move || {
-        let panel = Panel::build_pick_files(opt);
+        let panel = Panel::build_pick_files(&opt);
 
         let res = panel.run_modal();
         OutputFrom::from(&panel, res)
     })
 }
 
-pub fn pick_file_async<'a>(opt: &FileDialog<'a>) -> DialogFuture<Option<FileHandle>> {
-    let panel = Panel::build_pick_file(opt);
+pub fn pick_file_async(opt: FileDialog) -> DialogFuture<Option<FileHandle>> {
+    let panel = Panel::build_pick_file(&opt);
     AsyncDialog::new(panel).into()
 }
 
-pub fn save_file_async<'a>(opt: &FileDialog<'a>) -> DialogFuture<Option<FileHandle>> {
-    let panel = Panel::build_save_file(opt);
+pub fn save_file_async(opt: FileDialog) -> DialogFuture<Option<FileHandle>> {
+    let panel = Panel::build_save_file(&opt);
     AsyncDialog::new(panel).into()
 }
 
-pub fn pick_folder_async<'a>(opt: &FileDialog<'a>) -> DialogFuture<Option<FileHandle>> {
-    let panel = Panel::build_pick_folder(opt);
+pub fn pick_folder_async(opt: FileDialog) -> DialogFuture<Option<FileHandle>> {
+    let panel = Panel::build_pick_folder(&opt);
     AsyncDialog::new(panel).into()
 }
 
-pub fn pick_files_async<'a>(opt: &FileDialog<'a>) -> DialogFuture<Option<Vec<FileHandle>>> {
-    let panel = Panel::build_pick_files(opt);
+pub fn pick_files_async(opt: FileDialog) -> DialogFuture<Option<Vec<FileHandle>>> {
+    let panel = Panel::build_pick_files(&opt);
     AsyncDialog::new(panel).into()
 }
