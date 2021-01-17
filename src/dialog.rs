6da32fa7c4e70738c221acdab0a12547b9aace2b
Poly <marynczak.bartlomiej@gmail.com>
2021-01-17 01:47:32 +0100

    [Doc] Initial Docs

--- src/dialog.rs
+++ src/dialog.rs
@@ -8,6 +8,11 @@ pub(crate) struct Filter {
     pub extensions: Vec<String>,
 }
 
+/// ## Synchronous File Dialog
+/// #### Supported Platforms:
+/// - Linux
+/// - Windows
+/// - Mac
 #[derive(Default)]
 pub struct FileDialog {
     pub(crate) filters: Vec<Filter>,
@@ -15,10 +20,20 @@ pub struct FileDialog {
 }
 
 impl FileDialog {
+    /// New file dialog builder
     pub fn new() -> Self {
         Default::default()
     }
 
+    /// Add file extension filter.
+    ///
+    /// Takes in the name of the filter, and list of extensions
+    ///
+    /// #### Name of the filter will be displayed on supported platforms
+    /// - Windows
+    /// - Linux
+    ///
+    /// On platforms that don't support filter names, all filters will be merged into one filter
     pub fn add_filter(mut self, name: &str, extensions: &[&str]) -> Self {
         self.filters.push(Filter {
             name: name.into(),
@@ -27,6 +42,11 @@ impl FileDialog {
         self
     }
 
+    /// Set starting directory of the dialog.
+    /// #### Supported Platforms:
+    /// - Linux
+    /// - Windows
+    /// - Mac
     pub fn set_directory<P: AsRef<Path>>(mut self, path: &P) -> Self {
         self.starting_directory = Some(path.as_ref().into());
         self
@@ -35,38 +55,63 @@ impl FileDialog {
 
 #[cfg(not(target_arch = "wasm32"))]
 impl FileDialog {
+    /// Pick one file
     pub fn pick_file(self) -> Option<PathBuf> {
         crate::backend::pick_file(self)
     }
 
+    /// Pick multiple files
     pub fn pick_files(self) -> Option<Vec<PathBuf>> {
         crate::backend::pick_files(self)
     }
 
+    /// Pick one folder
     pub fn pick_folder(self) -> Option<PathBuf> {
         crate::backend::pick_folder(self)
     }
 
+    /// Opens save file dialog
     pub fn save_file(self) -> Option<PathBuf> {
         crate::backend::save_file(self)
     }
 }
 
+/// ## Asynchronous File Dialog
+/// #### Supported Platforms:
+/// - Linux
+/// - Windows
+/// - Mac
+/// - WASM32
 #[derive(Default)]
 pub struct AsyncFileDialog {
     file_dialog: FileDialog,
 }
 
 impl AsyncFileDialog {
+    /// New file dialog builder
     pub fn new() -> Self {
         Default::default()
     }
 
+    /// Add file extension filter.
+    ///
+    /// Takes in the name of the filter, and list of extensions
+    ///
+    /// #### Name of the filter will be displayed on supported platforms
+    /// - Windows
+    /// - Linux
+    ///
+    /// On platforms that don't support filter names, all filters will be merged into one filter
     pub fn add_filter(mut self, name: &str, extensions: &[&str]) -> Self {
         self.file_dialog = self.file_dialog.add_filter(name, extensions);
         self
     }
 
+    /// Set starting directory of the dialog.
+    /// #### Supported Platforms:
+    /// - Linux
+    /// - Windows
+    /// - Mac
     pub fn set_directory<P: AsRef<Path>>(mut self, path: &P) -> Self {
         self.file_dialog = self.file_dialog.set_directory(path);
         self
@@ -76,20 +121,28 @@ impl AsyncFileDialog {
 use std::future::Future;
 
 impl AsyncFileDialog {
+    /// Pick one file
     pub fn pick_file(self) -> impl Future<Output = Option<FileHandle>> {
         crate::backend::pick_file_async(self.file_dialog)
     }
 
+    /// Pick multiple files
     pub fn pick_files(self) -> impl Future<Output = Option<Vec<FileHandle>>> {
         crate::backend::pick_files_async(self.file_dialog)
     }
 
     #[cfg(not(target_arch = "wasm32"))]
+    /// Pick one folder
+    ///
+    /// Does not exist in `WASM32`
     pub fn pick_folder(self) -> impl Future<Output = Option<FileHandle>> {
         crate::backend::pick_folder_async(self.file_dialog)
     }
 
     #[cfg(not(target_arch = "wasm32"))]
+    /// Opens save file dialog
+    ///
+    /// Does not exist in `WASM32`
     pub fn save_file(self) -> impl Future<Output = Option<FileHandle>> {
         crate::backend::save_file_async(self.file_dialog)
     }
--- src/file_handle/native.rs
+++ src/file_handle/native.rs
@@ -60,10 +60,14 @@ impl Future for Reader {
 pub struct FileHandle(PathBuf);
 
 impl FileHandle {
+    /// On native platforms it wraps path.
+    ///
+    /// On `WASM32` it wraps JS `File` object.
     pub fn wrap(path_buf: PathBuf) -> Self {
         Self(path_buf)
     }
 
+    /// Get name of a file
     pub fn file_name(&self) -> String {
         self.0
             .file_name()
@@ -72,15 +76,29 @@ impl FileHandle {
             .unwrap_or_default()
     }
 
+    /// Gets path to a file.
+    ///
+    /// Does not exist in `WASM32`
     pub fn path(&self) -> &Path {
         &self.0
     }
 
-    /// This fn exists souly to keep native api in pair with async only web api.
+    /// Reads a file asynchronously.
+    ///
+    /// On native platforms it spawns a `std::thread` in the background.
+    ///
+    /// `This fn exists souly to keep native api in pair with async only web api.`
     pub async fn read(&self) -> Vec<u8> {
         Reader::new(&self.0).await
     }
 
+    /// Unwraps a `FileHandle` and returns innet type.
+    ///
+    /// It should be used, if user wants to handle file read themselves
+    ///
+    /// On native platforms returns path.
+    ///
+    /// On `WASM32` it returns JS `File` object.
     pub fn inner(&self) -> &Path {
         &self.0
     }
@@ -88,6 +106,6 @@ impl FileHandle {
 
 impl std::fmt::Debug for FileHandle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.file_name())
+        write!(f, "{:?}", self.path())
     }
 }
