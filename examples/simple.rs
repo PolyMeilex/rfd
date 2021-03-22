7c9a88d9a8aae1268fed5bc6c8ffbdff9f66672e
Poly <marynczak.bartlomiej@gmail.com>
2021-03-22 15:42:56 +0100

    Fix example #9

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,9 +1,11 @@
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    let path = std::env::current_dir().unwrap();
+
     let res = rfd::FileDialog::new()
         .add_filter("text", &["txt", "rs"])
         .add_filter("rust", &["rs", "toml"])
-        .set_directory(&"/")
+        .set_directory(&path)
         .pick_files();
 
     println!("The user choose: {:#?}", res);
