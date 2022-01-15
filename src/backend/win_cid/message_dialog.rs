ee8a843b9515aabc62f1179f08b3d4198a9e23be
Lucas Nogueira <lucas@tauri.studio>
2022-01-15 13:24:09 -0300

    chore(deps): update `windows` to 0.30

--- src/backend/win_cid/message_dialog.rs
+++ src/backend/win_cid/message_dialog.rs
@@ -50,7 +50,7 @@ impl WinMessageDialog {
 
         #[cfg(feature = "parent")]
         let parent = match opt.parent {
-            Some(RawWindowHandle::Win32(handle)) => Some(handle.hwnd as HWND),
+            Some(RawWindowHandle::Win32(handle)) => Some(HWND(handle.hwnd as _)),
             None => None,
             _ => unreachable!("unsupported window handle, expected: Windows"),
         };
