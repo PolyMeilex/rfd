3c9f3c7cbf57e4d390b9e8b7c5cc3368ac1fdb84
Poly <marynczakbartlomiej@gmail.com>
2022-01-29 18:04:36 +0100

    Initial xdg and gtk3 split

--- build.rs
+++ build.rs
@@ -5,9 +5,9 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=AppKit");
     }
 
-    // if std::env::var("TARGET").unwrap().contains("-ios") {
-    //     println!("cargo:rustc-link-lib=framework=UIKit");
-    // } else {
-    //     println!("cargo:rustc-link-lib=framework=AppKit");
-    // }
+    #[cfg(all(feature = "gtk3", feature = "xdg-portal"))]
+    compile_error!("You can't enable both GTK3 & XDG Portal features at once");
+
+    #[cfg(not(any(feature = "gtk3", feature = "xdg-portal")))]
+    compile_error!("You need to choose at least one backend: `gtk3` or `xdg-portal` features");
 }
