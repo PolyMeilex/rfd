b4c428064a89ca3c1896fcd639ab74293c3777f0
Amr Bashir <amr.bashir2015@gmail.com>
2022-11-07 14:35:30 +0200

    Update file_dialog.rs

--- src/file_dialog.rs
+++ src/file_dialog.rs
@@ -62,7 +62,7 @@ impl FileDialog {
         if path.to_str().map(|p|p.is_empty()).unwrap_or(false) {
             path = Path::new(".")
         }
-        self.starting_directory = Some(path.as_ref().into());
+        self.starting_directory = Some(path.into());
         self
     }
 
