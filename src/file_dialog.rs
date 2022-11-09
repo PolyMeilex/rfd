ad6d2a41c4e0b694dc9ce7c37ba22e644c8a8c43
Amr Bashir <amr.bashir2015@gmail.com>
2022-11-09 20:47:15 +0200

    fallback to `None`

--- src/file_dialog.rs
+++ src/file_dialog.rs
@@ -58,11 +58,12 @@ impl FileDialog {
     ///   * Windows
     ///   * Mac
     pub fn set_directory<P: AsRef<Path>>(mut self, path: P) -> Self {
-        let mut path = path.as_ref();
+        let path = path.as_ref();
         if path.to_str().map(|p|p.is_empty()).unwrap_or(false) {
-            path = Path::new(".")
+            self.starting_directory = None;
+        } else {
+            self.starting_directory = Some(path.into());
         }
-        self.starting_directory = Some(path.into());
         self
     }
 
