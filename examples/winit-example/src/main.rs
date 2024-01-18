980ce7932292271e48967e49837f87cb6315f6a2
dtzxporter <dtzxporter@users.noreply.github.com>
2024-01-18 15:17:50 -0500

    Update example to latest winit for rwh 0.6.

--- examples/winit-example/src/main.rs
+++ examples/winit-example/src/main.rs
@@ -1,6 +1,7 @@
 use winit::{
     event::{self, WindowEvent},
-    event_loop::{ControlFlow, EventLoopBuilder},
+    event_loop::EventLoopBuilder,
+    keyboard::{KeyCode, PhysicalKey},
 };
 
 use wasm_bindgen::prelude::*;
@@ -11,7 +12,9 @@ extern "C" {
 }
 
 fn main() {
-    let event_loop = EventLoopBuilder::<String>::with_user_event().build();
+    let event_loop = EventLoopBuilder::<String>::with_user_event()
+        .build()
+        .expect("Failed");
     let builder = winit::window::WindowBuilder::new();
 
     let window = builder.build(&event_loop).unwrap();
@@ -32,111 +35,113 @@ fn main() {
     let event_loop_proxy = event_loop.create_proxy();
     let executor = Executor::new();
 
-    event_loop.run(move |event, _, control_flow| match event {
-        event::Event::UserEvent(name) => {
-            #[cfg(target_arch = "wasm32")]
-            alert(&name);
-            #[cfg(not(target_arch = "wasm32"))]
-            println!("{}", name);
-        }
-        event::Event::WindowEvent { event, .. } => match event {
-            WindowEvent::CloseRequested { .. } => *control_flow = ControlFlow::Exit,
-            WindowEvent::KeyboardInput {
-                input:
-                    event::KeyboardInput {
-                        state: event::ElementState::Pressed,
-                        virtual_keycode: Some(winit::event::VirtualKeyCode::S),
-                        ..
-                    },
-                ..
-            } => {
-                let dialog = rfd::AsyncFileDialog::new()
-                    .add_filter("midi", &["mid", "midi"])
-                    .add_filter("rust", &["rs", "toml"])
-                    .set_parent(&window)
-                    .save_file();
-
-                let event_loop_proxy = event_loop_proxy.clone();
-                executor.execut(async move {
-                    let file = dialog.await;
-
-                    let file = if let Some(file) = file {
-                        file.write(b"Hi! This is a test file").await.unwrap();
-                        Some(file)
-                    } else {
-                        None
-                    };
-
-                    event_loop_proxy
-                        .send_event(format!("saved file name: {:#?}", file))
-                        .ok();
-                });
+    event_loop
+        .run(move |event, target| match event {
+            event::Event::UserEvent(name) => {
+                #[cfg(target_arch = "wasm32")]
+                alert(&name);
+                #[cfg(not(target_arch = "wasm32"))]
+                println!("{}", name);
             }
-            WindowEvent::KeyboardInput {
-                input:
-                    event::KeyboardInput {
-                        state: event::ElementState::Pressed,
-                        virtual_keycode: Some(winit::event::VirtualKeyCode::F),
-                        ..
-                    },
-                ..
-            } => {
-                let dialog = rfd::AsyncFileDialog::new()
-                    .add_filter("midi", &["mid", "midi"])
-                    .add_filter("rust", &["rs", "toml"])
-                    .set_parent(&window)
-                    .pick_file();
+            event::Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested { .. } => target.exit(),
+                WindowEvent::KeyboardInput {
+                    event:
+                        event::KeyEvent {
+                            state: event::ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::KeyS),
+                            ..
+                        },
+                    ..
+                } => {
+                    let dialog = rfd::AsyncFileDialog::new()
+                        .add_filter("midi", &["mid", "midi"])
+                        .add_filter("rust", &["rs", "toml"])
+                        .set_parent(&window)
+                        .save_file();
 
-                let event_loop_proxy = event_loop_proxy.clone();
-                executor.execut(async move {
-                    let files = dialog.await;
+                    let event_loop_proxy = event_loop_proxy.clone();
+                    executor.execut(async move {
+                        let file = dialog.await;
 
-                    // let names: Vec<String> = files.into_iter().map(|f| f.file_name()).collect();
-                    let names = files;
+                        let file = if let Some(file) = file {
+                            file.write(b"Hi! This is a test file").await.unwrap();
+                            Some(file)
+                        } else {
+                            None
+                        };
 
-                    event_loop_proxy.send_event(format!("{:#?}", names)).ok();
-                });
-            }
-            WindowEvent::DroppedFile(file_path) => {
-                let dialog = rfd::AsyncMessageDialog::new()
-                    .set_title("File dropped")
-                    .set_description(&format!("file path was: {:#?}", file_path))
-                    .set_buttons(rfd::MessageButtons::YesNo)
-                    .set_parent(&window)
-                    .show();
+                        event_loop_proxy
+                            .send_event(format!("saved file name: {:#?}", file))
+                            .ok();
+                    });
+                }
+                WindowEvent::KeyboardInput {
+                    event:
+                        event::KeyEvent {
+                            state: event::ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::KeyF),
+                            ..
+                        },
+                    ..
+                } => {
+                    let dialog = rfd::AsyncFileDialog::new()
+                        .add_filter("midi", &["mid", "midi"])
+                        .add_filter("rust", &["rs", "toml"])
+                        .set_parent(&window)
+                        .pick_file();
 
-                let event_loop_proxy = event_loop_proxy.clone();
-                executor.execut(async move {
-                    let val = dialog.await;
-                    event_loop_proxy.send_event(format!("Msg: {}", val)).ok();
-                });
-            }
-            WindowEvent::KeyboardInput {
-                input:
-                    event::KeyboardInput {
-                        state: event::ElementState::Pressed,
-                        virtual_keycode: Some(winit::event::VirtualKeyCode::M),
-                        ..
-                    },
-                ..
-            } => {
-                let dialog = rfd::AsyncMessageDialog::new()
-                    .set_title("Msg!")
-                    .set_description("Description!")
-                    .set_buttons(rfd::MessageButtons::YesNo)
-                    .set_parent(&window)
-                    .show();
+                    let event_loop_proxy = event_loop_proxy.clone();
+                    executor.execut(async move {
+                        let files = dialog.await;
 
-                let event_loop_proxy = event_loop_proxy.clone();
-                executor.execut(async move {
-                    let val = dialog.await;
-                    event_loop_proxy.send_event(format!("Msg: {}", val)).ok();
-                });
-            }
+                        // let names: Vec<String> = files.into_iter().map(|f| f.file_name()).collect();
+                        let names = files;
+
+                        event_loop_proxy.send_event(format!("{:#?}", names)).ok();
+                    });
+                }
+                WindowEvent::DroppedFile(file_path) => {
+                    let dialog = rfd::AsyncMessageDialog::new()
+                        .set_title("File dropped")
+                        .set_description(format!("file path was: {:#?}", file_path))
+                        .set_buttons(rfd::MessageButtons::YesNo)
+                        .set_parent(&window)
+                        .show();
+
+                    let event_loop_proxy = event_loop_proxy.clone();
+                    executor.execut(async move {
+                        let val = dialog.await;
+                        event_loop_proxy.send_event(format!("Msg: {}", val)).ok();
+                    });
+                }
+                WindowEvent::KeyboardInput {
+                    event:
+                        event::KeyEvent {
+                            state: event::ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::KeyM),
+                            ..
+                        },
+                    ..
+                } => {
+                    let dialog = rfd::AsyncMessageDialog::new()
+                        .set_title("Msg!")
+                        .set_description("Description!")
+                        .set_buttons(rfd::MessageButtons::YesNo)
+                        .set_parent(&window)
+                        .show();
+
+                    let event_loop_proxy = event_loop_proxy.clone();
+                    executor.execut(async move {
+                        let val = dialog.await;
+                        event_loop_proxy.send_event(format!("Msg: {}", val)).ok();
+                    });
+                }
+                _ => {}
+            },
             _ => {}
-        },
-        _ => {}
-    });
+        })
+        .unwrap();
 }
 
 use std::future::Future;
