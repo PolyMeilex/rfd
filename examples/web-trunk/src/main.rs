1b57cc8ca3374de103621636613a0c466259b8ab
Kevin Mehall <km@kevinmehall.net>
2025-11-12 21:29:49 -0700

    examples/web-trunk: Clean up unusable cfg code
    The web_sys types only exist on wasm, so the whole
    example will not work on other targets

--- examples/web-trunk/src/main.rs
+++ examples/web-trunk/src/main.rs
@@ -18,14 +18,10 @@ fn main() {
         let task = rfd::AsyncFileDialog::new().pick_file();
 
         // Await somewhere else
-        execute(async {
+        wasm_bindgen_futures::spawn_local(async {
             let file = task.await;
 
             if let Some(file) = file {
-                // If you are on native platform you can just get the path
-                #[cfg(not(target_arch = "wasm32"))]
-                println!("{:?}", file.path());
-
                 // If you care about wasm support you just read() the file
                 file.read().await;
             }
@@ -33,16 +29,4 @@ fn main() {
     }).into_js_value();
 
     button.set_onclick(Some(&onclick.as_ref().unchecked_ref()));
-}
-
-use std::future::Future;
-
-#[cfg(not(target_arch = "wasm32"))]
-fn execute<F: Future<Output = ()> + Send + 'static>(f: F) {
-    // this is stupid... use any executor of your choice instead
-    std::thread::spawn(move || futures::executor::block_on(f));
-}
-#[cfg(target_arch = "wasm32")]
-fn execute<F: Future<Output = ()> + 'static>(f: F) {
-    wasm_bindgen_futures::spawn_local(f);
 }
