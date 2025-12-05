11c7544a0d9f9e31299e26b42f21156850da7435
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-12-06 00:21:06 +0100

    chore: Fix clippy lints

--- examples/msg.rs
+++ examples/msg.rs
@@ -1,31 +1,4 @@
-use std::io::{self, Read};
-
 fn main() {
-    #[cfg(all(
-        any(
-            target_os = "linux",
-            target_os = "freebsd",
-            target_os = "dragonfly",
-            target_os = "netbsd",
-            target_os = "openbsd"
-        ),
-        not(feature = "gtk3")
-    ))]
-    let res = "";
-    #[cfg(any(
-        target_os = "windows",
-        target_os = "macos",
-        all(
-            any(
-                target_os = "linux",
-                target_os = "freebsd",
-                target_os = "dragonfly",
-                target_os = "netbsd",
-                target_os = "openbsd"
-            ),
-            feature = "gtk3"
-        )
-    ))]
     let res = rfd::MessageDialog::new()
         .set_title("Msg!")
         .set_description("Description!")
@@ -34,29 +7,6 @@ fn main() {
         .show();
     println!("res: {res}");
 
-    #[cfg(windows)]
-    println!("Ctrl+Z");
-    #[cfg(not(windows))]
-    println!("Ctrl+D");
-
-    let mut stdin = io::stdin();
-    let mut buffer: Vec<u8> = vec![];
-    stdin.read_to_end(&mut buffer).unwrap();
-
-    #[cfg(any(
-        target_os = "windows",
-        target_os = "macos",
-        all(
-            any(
-                target_os = "linux",
-                target_os = "freebsd",
-                target_os = "dragonfly",
-                target_os = "netbsd",
-                target_os = "openbsd"
-            ),
-            feature = "gtk3"
-        )
-    ))]
     futures::executor::block_on(async move {
         let res = rfd::AsyncMessageDialog::new()
             .set_title("Msg!")
@@ -66,13 +16,4 @@ fn main() {
             .await;
         println!("res: {res}");
     });
-
-    #[cfg(windows)]
-    println!("Ctrl+Z");
-    #[cfg(not(windows))]
-    println!("Ctrl+D");
-    let mut stdin = io::stdin();
-    let mut buffer: Vec<u8> = vec![];
-    stdin.read_to_end(&mut buffer).unwrap();
-    println!();
 }
