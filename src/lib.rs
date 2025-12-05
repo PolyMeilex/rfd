f71cb2160d497347d5a639ea92fa887264e04764
Bartłomiej Maryńczak <marynczak.bartlomiej@gmail.com>
2025-12-06 00:18:41 +0100

    linux: Drop manual async implementations (#284)

--- src/lib.rs
+++ src/lib.rs
@@ -133,7 +133,6 @@ mod file_handle;
 pub use file_handle::FileHandle;
 
 mod file_dialog;
-#[cfg(any(target_os = "macos", target_os = "windows"))]
 mod oneshot;
 
 #[cfg(not(target_arch = "wasm32"))]
